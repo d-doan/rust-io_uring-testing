5587f59bf2863392e9e57fc5458f075b3a86d834
d-doan <dandoan03@gmail.com>
2026-02-21 14:29:33 -0800

    refactor unsafe blocks to necessary scope

--- io_uring_testing/src/main.rs
+++ io_uring_testing/src/main.rs
@@ -1,7 +1,8 @@
 use io_uring::{opcode, types, IoUring};
-use libc::{mmap, munmap, MAP_ANONYMOUS, MAP_PRIVATE, PROT_READ, PROT_WRITE, iovec};
+use libc::{iovec};
+use std::fs::{File, OpenOptions};
 use std::os::unix::io::AsRawFd;
-use std::{ptr, fs::File, io::Read};
+use std::{io::Read};
 
 // desync kernel from application
 // pinning allows kernel to bypass standard page table checks
@@ -21,48 +22,51 @@ use std::{ptr, fs::File, io::Read};
 // check if registering + dropping vec<u8> works same way?
 
 fn main() {
+    // allocate memory and store secret at addr
+    let size = 1024 * 1024;
+    let mut v = vec![0u8; size];
+    let secret = b"secret zombie data";
+    v[..secret.len()].copy_from_slice(secret);
+    println!("wrote secret to memory: {:?}", std::str::from_utf8(secret).unwrap());
+
+    let ptr = v.as_ptr();
+
+    // pin it with fixed buffer
+    // pinning increments ref count on physical RAM
+    let mut ring = IoUring::new(8).expect("ring failed");
+    let out_file = OpenOptions::new()
+                    .write(true).create(true).truncate(true)
+                    .open("/tmp/zombie_output.txt").unwrap();
+    let out_fd = types::Fd(out_file.as_raw_fd());
+
+    println!("Pinning vector (increases ref count)");
     unsafe {
-        // allocate memory and store secret at addr
-        let mut v = vec![0u8; 4096];
-        let secret = b"secret zombie data";
-        v[..secret.len()].copy_from_slice(secret);
-        println!("wrote secret to memory: {:?}", std::str::from_utf8(secret).unwrap());
-
-        let ptr = v.as_ptr();
-        let len = v.len();
-
-        // pin it with fixed buffer
-        // pinning increments ref count on physical RAM
-        let iov = iovec { iov_base: ptr as *mut _, iov_len: len };
-        let mut ring = IoUring::new(8).expect("ring failed");
+        let iov = iovec {iov_base: ptr as *mut _, iov_len: size};
         ring.submitter().register_buffers(&[iov]).expect("register failed");
+    }
 
-        // 'free' memory, memory should be gone in rust's eyes
-        drop(v);
+    println!("Dropping vector");
+    // 'free' memory, memory should be gone in rust's eyes
+    drop(v);
 
-        let file = File::create("/tmp/zombie_output.txt").unwrap();
-        let fd = types::Fd(file.as_raw_fd());
-        println!("ask kernel to get secret from zombie page");
-
-        // writes from the registered buffer to the file descriptor.
-        // kernel thread is able to write our 'zombie data' even though process shouldn't have mapping
-        let write_op = opcode::WriteFixed::new(fd, ptr, secret.len() as u32, 0).build();
+    unsafe {
+        let write_op = opcode::WriteFixed::new(out_fd, ptr, secret.len() as u32, 0).build();
         ring.submission().push(&write_op).expect("queue full");
-        ring.submit_and_wait(1).unwrap();
+    }
 
-        // check results
-        let mut check_file = File::open("/tmp/zombie_output.txt").unwrap();
-        let mut contents = String::new();
-        check_file.read_to_string(&mut contents).unwrap();
+    ring.submit_and_wait(1).unwrap();
 
-        println!("\ncomparing expected versus what kernel read");
-        println!("expected: {:?}", std::str::from_utf8(secret).unwrap());
-        println!("actual:   {:?}", contents);
+    // check results
+    let mut check_file = File::open("/tmp/zombie_output.txt").unwrap();
+    let mut contents = String::new();
+    check_file.read_to_string(&mut contents).unwrap();
 
-        if contents.as_bytes() == secret {
-            println!("side channel storage enabled");
-        } else {
-            println!("data got lost or corrupted");
-        }
+    println!("expected: {:?}", std::str::from_utf8(secret).unwrap());
+    println!("actual:   {:?}", contents);
+
+    if contents.as_bytes() == secret {
+        println!("side channel storage enabled");
+    } else {
+        println!("data got lost or corrupted");
     }
 }
